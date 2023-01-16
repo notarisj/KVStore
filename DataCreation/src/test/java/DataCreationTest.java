f8502efe60d8c3946a993c8cd8eab25ceef94240
Notaris <80954329+notarisj@users.noreply.github.com>
2023-01-16 11:55:37 +0200

    Fixed dataToIndex file was not replaced and remove count logic from getRandomAttribute because it forced to use UUID

--- DataCreation/src/test/java/DataCreationTest.java
+++ DataCreation/src/test/java/DataCreationTest.java
@@ -11,7 +11,7 @@ public class DataCreationTest {
         DateFormat df = new SimpleDateFormat("mm:ss:SSS");
 
         long startTime = System.currentTimeMillis();
-        String[] args = {"-n", "1000", "-m", "5", "-d", "4", "-l" , "4", "-k", System.getProperty("user.home") + "/Desktop/keyFile.txt"};
+        String[] args = {"-n", "1000", "-m", "5", "-d", "3", "-l" , "4", "-k", System.getProperty("user.home") + "/Desktop/keyFile.txt"};
         DataCreation.main(args);
         long endTime = System.currentTimeMillis();
         System.out.println("Time to execute: " + df.format(endTime - startTime));
